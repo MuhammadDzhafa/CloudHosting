dd29db1e199684fd010ceca4a5992853aa4e16c1
adindafaayza <afm243.sch@gmail.com>
2024-10-02 15:14:32 +0700

    Fix Section13 Footer

--- resources/views/app/hosting-plans/landing-page/section13.blade.php
+++ resources/views/app/hosting-plans/landing-page/section13.blade.php
@@ -3,7 +3,54 @@
     <div class="footer-container">
       <div class="footer-column">
         <img src="assets/img/logos/logo/awanhosting.svg" alt="awanhosting" class="w-full max-w-[191px]">
-        <p class="font-inter font-semibold text-[14px] text-[#263E83]">Lift Your Website to the Cloud</p>
+        <div class="footer-column">
+          <ul>
+            <li>
+              <p class="footer-content has-icon no-hover">
+                <img src="assets/img/section13/gridicons--phone.svg" alt="phone" style="width:18px; height: 18px;">
+                022-2010606
+              </p>
+            </li>
+            <li>
+              <p class="footer-content has-icon no-hover">
+                <img src="assets/img/section13/material-symbols--phone-enabled-outline.svg" alt="phone"
+                  style="width:18px; height: 18px;">
+                +62 811 2222 656
+              </p>
+            </li>
+            <li>
+              <p class="footer-content has-icon no-hover">
+                <img src="assets/img/section13/ic--outline-email.svg" alt="email" style="width:18px; height: 18px;">
+                info@kazee.id
+              </p>
+            </li>
+            <li>
+              <p class="footer-content has-icon no-hover">
+                <img src="assets/img/section13/humbleicons--location.svg" alt="location"
+                  style="width:18px; height: 18px;">
+                Jl. Setrasari Indah No. 4, Bandung
+              </p>
+            </li>
+          </ul>
+
+          <div class="flex flex-row gap-4 items-center">
+            <a href="#">
+              <img src="assets/img/section13/Facebook.svg" alt="facebook">
+            </a>
+            <a href="#">
+              <img src="assets/img/section13/Twitter.svg" alt="twitter">
+            </a>
+            <a href="#">
+              <img src="assets/img/section13/Instagram.svg" alt="instagram">
+            </a>
+            <a href="#">
+              <img src="assets/img/section13/YouTube.svg" alt="youtube">
+            </a>
+            <a href="#">
+              <img src="assets/img/section13/LinkedIn.svg" alt="linkedin">
+            </a>
+          </div>
+        </div>
       </div>
       <div class="footer-column">
         <h4 class="footer-header">Products</h4>
@@ -45,55 +92,6 @@
             <a href="#">Privacy Policy</a>
           </li>
         </ul>
-      </div>
-      <div class="footer-column">
-        <h4 class="footer-header">Contact Us</h4>
-        <ul>
-          <li>
-            <p class="footer-content has-icon no-hover">
-              <img src="assets/img/section13/gridicons--phone.svg" alt="phone" style="width:18px; height: 18px;">
-              022-2010606
-            </p>
-          </li>
-          <li>
-            <p class="footer-content has-icon no-hover">
-              <img src="assets/img/section13/material-symbols--phone-enabled-outline.svg" alt="phone"
-                style="width:18px; height: 18px;">
-              +62 811 2222 656
-            </p>
-          </li>
-          <li>
-            <p class="footer-content has-icon no-hover">
-              <img src="assets/img/section13/ic--outline-email.svg" alt="email" style="width:18px; height: 18px;">
-              info@kazee.id
-            </p>
-          </li>
-          <li>
-            <p class="footer-content has-icon no-hover">
-              <img src="assets/img/section13/humbleicons--location.svg" alt="location"
-                style="width:18px; height: 18px;">
-              Jl. Setrasari Indah No. 4, Bandung
-            </p>
-          </li>
-        </ul>
-
-        <div class="flex flex-row gap-4 items-center">
-          <a href="#">
-            <img src="assets/img/section13/Facebook.svg" alt="facebook">
-          </a>
-          <a href="#">
-            <img src="assets/img/section13/Twitter.svg" alt="twitter">
-          </a>
-          <a href="#">
-            <img src="assets/img/section13/Instagram.svg" alt="instagram">
-          </a>
-          <a href="#">
-            <img src="assets/img/section13/YouTube.svg" alt="youtube">
-          </a>
-          <a href="#">
-            <img src="assets/img/section13/LinkedIn.svg" alt="linkedin">
-          </a>
-        </div>
       </div>
     </div>
 
