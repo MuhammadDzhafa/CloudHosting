49d6d4ac165c7e87d6d113ff45aca8cc73314b2c
Tabitha <tabithasalsa@gmail.com>
2024-10-03 14:21:50 +0700

    fix faq page

--- resources/views/app/hosting-plans/faq/section1.blade.php
+++ resources/views/app/hosting-plans/faq/section1.blade.php
@@ -1,5 +1,5 @@
-<div class="section-frame padding-4 bg-gradient-custom flex-row" style="height: 200px; display: flex; justify-content: center; align-items: center;">
-    <h2 class="title-section text-white">
+<div class="section-frame bg-gradient-custom flex-row" style="height: 200px; display: flex; justify-content: center; align-items: center;">
+    <h2 class="title-hero title-primary text-white">
         FAQ's
     </h2>
 </div>
