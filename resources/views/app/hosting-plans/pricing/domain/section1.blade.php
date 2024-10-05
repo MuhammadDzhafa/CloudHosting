67f88bb36023446dc27c7d9a617a64ee94a0ee12
MuhammadDzhafa <muhammaddzhafa@gmail.com>
2024-10-05 16:45:09 +0700

    fix cloud-hosting

--- resources/views/app/hosting-plans/pricing/domain/section1.blade.php
+++ resources/views/app/hosting-plans/pricing/domain/section1.blade.php
@@ -1,17 +1,16 @@
-<div class="section-frame padding-1">
+<div class="section-frame padding-1" style="background: #fff;">
     <div class="flex flex-wrap items-center">
         <div class="w-full lg:w-1/2">
-            <h1 class="text-4xl font-bold text-purple-600 mb-2">
+            <h1 class="font-inter text-[50px] font-bold leading-[60px] text-gradient mb-4 text-left">
                 Find Your Perfect Domain
             </h1>
-            <p class="text-gray-600 mb-4">
+
+            <p class="w-[581px] font-inter text-[18px] font-normal leading-[23.4px] text-[#283252] text-left mb-10">
                 Find your ideal domain effortlessly with our advanced search tool and domain recommendation feature. Get personalized suggestions based on your preferences, explore available names, and secure the perfect match for your brand.
             </p>
-            <a href="#" class="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700">
-                Check
-                <svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-                </svg>
+
+            <a class="button h-button bg-[#4A6DCB] hover:bg-[#395FC6] active:bg-[#3253AE] text-white hover:text-white active:text-white rounded-full mr-[10px]">Check
+                <img src="assets/img/right.svg" alt="">
             </a>
         </div>
         <div class="w-full lg:w-1/2 lg:mt-0">
